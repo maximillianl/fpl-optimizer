f0648d8c13fb174fce604c57e353992061e93ddb
maximillianl <125389788+maximillianl@users.noreply.github.com>
2024-11-10 16:57:23 -0500

    Update main.py
    fixed formation input while logic
    changed it from automatically giving value scores for all the positions to asking user which position they want to see

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ raw_general_data = general_data()
 
 
 # Prompt for budget input
-budget_input = input("Enter your budget: ")
+budget_input = input("Enter your budget (default is 100.0): ")
 
 # Initial check and validation
 while True:
@@ -25,38 +25,55 @@ while True:
             print("Invalid input. Please enter a numeric value.")
 
     # Prompt again if input was invalid
-    budget_input = input("Enter your budget (default is 100): ")
+    budget_input = input("Enter your budget (default is 100.0): ")
     
 
 
+possible_formations = [
+        (3, 5, 2),
+        (3, 4, 3),
+        (4, 3, 3),
+        (4, 4, 2),
+        (4, 5, 1),
+        (5, 3, 2),
+        (5, 4, 1),
+    ]  
 
-
-formation_input = input("Enter your formation (#-#-#): ")
+formation_input = input("Enter your formation (#-#-#) (Leave blank for optimal formation): ")
 while True:
     try:
         formation = tuple(map(int, formation_input.split('-')))
-        if sum(formation) == 10 and len(formation) == 3:
+        if sum(formation) == 10 and len(formation) == 3 and formation in possible_formations:
             break
         else:
-            print("Formation must have 10 players.")
+            print("Formation must be a valid FPL formation and in the format #-#-#.")
     except ValueError:
         if formation_input == '' or formation_input == None:
             formation = ''
             break
         else:
             print("Invalid input. Please enter a numeric value.")
-            formation_input = input("Enter your formation (#-#-#): ")
+    formation_input = input("Enter your formation (#-#-#) (Leave blank for optimal formation): ")
 
 
     
-
 
 if raw_general_data is not None:
     cleaned_general_data = prepare_data(raw_general_data)
     optimize_team(raw_general_data, budget, formation)
     
-    print(calculations_gk(cleaned_general_data))
-    print(calculations_def(cleaned_general_data))
-    print(calculations_mid(cleaned_general_data))
-    print(calculations_fwd(cleaned_general_data))
-
+    while True:
+        print("Enter the position for which you would like to see the value scores (GK, DEF, MID, FWD):")
+        position_input = input("Type anything else to exit the program: ")
+        position_input = position_input.upper()
+        if position_input == 'GK':
+            print(calculations_gk(cleaned_general_data))
+        elif position_input == 'DEF':
+            print(calculations_def(cleaned_general_data))
+        elif position_input == 'MID':
+            print(calculations_mid(cleaned_general_data))
+        elif position_input == 'FWD':
+            print(calculations_fwd(cleaned_general_data))
+        else:
+            break
+    
